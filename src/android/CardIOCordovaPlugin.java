da02467d3ed6650a638f5b109bda0e2b86f42cb4
Jose Medaglia <Jose Medaglia>
2016-05-15 19:21:44 -0600

    Added support to new runtime permissions model of Android M

--- src/android/CardIOCordovaPlugin.java
+++ src/android/CardIOCordovaPlugin.java
@@ -4,38 +4,50 @@ package io.card.cordova.sdk;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.net.Uri;
+import android.os.Bundle;
 
 import io.card.payment.CardIOActivity;
 import io.card.payment.CreditCard;
 
 public class CardIOCordovaPlugin extends CordovaPlugin {
 
+    private JSONArray executeArgs = null;
     private CallbackContext callbackContext;
     private Activity activity = null;
     private static final int REQUEST_CARD_SCAN = 10;
+    private static final int PERMISSION_DENIED_ERROR = 20;
+
+    private static final String[] permissions = { Manifest.permission.VIBRATE, Manifest.permission.CAMERA };
 
     @Override
     public boolean execute(String action, JSONArray args,
                            CallbackContext callbackContext) throws JSONException {
+        this.executeArgs = args;
         this.callbackContext = callbackContext;
         this.activity = this.cordova.getActivity();
         boolean retValue = true;
         if (action.equals("scan")) {
             this.scan(args);
         } else if (action.equals("canScan")) {
-            this.canScan(args);
+            requestMissingPermissions(REQUEST_CARD_SCAN, this.permissions);
         } else if (action.equals("version")) {
             this.callbackContext.success(CardIOActivity.sdkVersion());
         } else {
@@ -43,6 +55,45 @@ public class CardIOCordovaPlugin extends CordovaPlugin {
         }
 
         return retValue;
+    }
+
+    /**
+     * Executes Cordova request after ensuring required permissions have been granted.
+     * 
+     * @param requestCode Identifier of the request to execute.
+     */
+    private void executeWithPermissions(int requestCode) {
+        final CallbackContext callbackContext = this.callbackContext;
+        final JSONArray executeArgs = this.executeArgs;
+        
+        switch(requestCode) {
+            case REQUEST_CARD_SCAN:
+                this.canScan(executeArgs);
+                break;
+        }
+    }
+
+    /**
+     * Ensures that a given set of permissions is granted before executing the request.
+     * 
+     * @param requestCode Identifier of the request to execute.
+     * @param permissions Permission names to request.
+     */
+    private void requestMissingPermissions(int requestCode, String[] permissions) {
+        ArrayList<String> missingPermissions = new ArrayList(permissions.length);
+        //Builds the list of missing permissions
+        for(String permission:permissions) {
+            if (!PermissionHelper.hasPermission(this, permission)) {
+                missingPermissions.add(permission);
+            }
+        }
+        
+        // Requests permissions if needed, otherwise execute request
+        if (missingPermissions.isEmpty()) {
+            executeWithPermissions(requestCode);
+        } else {
+            PermissionHelper.requestPermissions(this, requestCode, missingPermissions.toArray(new String[0]));
+        }
     }
 
     @Override
@@ -77,7 +128,7 @@ public class CardIOCordovaPlugin extends CordovaPlugin {
         this.cordova.startActivityForResult(this, scanIntent, REQUEST_CARD_SCAN);
     }
 
-    private void canScan(JSONArray args) throws JSONException {
+    private void canScan(JSONArray args) {
         if (CardIOActivity.canReadCardWithCamera()) {
             // This is where we return if scanning is enabled.
             this.callbackContext.success("Card Scanning is enabled");
@@ -105,6 +156,25 @@ public class CardIOCordovaPlugin extends CordovaPlugin {
                 this.callbackContext.error(resultCode);
             }
         }
+    }
+
+	// onRequestPermissionResult
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        for(int r:grantResults) {
+            if(r == PackageManager.PERMISSION_DENIED) {
+                this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+                return;
+            }
+        }
+        
+        //Execute request with permissions granted
+        executeWithPermissions(requestCode);
+    }
+
+    // onRestoreStateForActivityResult
+    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
     }
 
     private JSONObject toJSONObject(CreditCard card) {
